ac7a345f3b28ef48262286fb343a1d5ef2116b2e
xoma_star <40947642+xoma-star@users.noreply.github.com>
2020-05-07 20:17:00 +0500

    Updates

---
+++ precache-manifest.23199c0fa5fbbde1c85fe3298f741773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b274266795a85ec65638acd4ae43ca6c",
+    "url": "/game1/index.html"
+  },
+  {
+    "revision": "3ede392461b38cf97b5c",
+    "url": "/game1/static/css/2.a140de60.chunk.css"
+  },
+  {
+    "revision": "58f50957466bafd0cdf5",
+    "url": "/game1/static/css/main.5041596a.chunk.css"
+  },
+  {
+    "revision": "3ede392461b38cf97b5c",
+    "url": "/game1/static/js/2.389f1c50.chunk.js"
+  },
+  {
+    "revision": "49a95be1b8a297c86c4b94d472801c51",
+    "url": "/game1/static/js/2.389f1c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f50957466bafd0cdf5",
+    "url": "/game1/static/js/main.3ecab6d3.chunk.js"
+  },
+  {
+    "revision": "de184dbccaf894dd332d",
+    "url": "/game1/static/js/runtime-main.0f06a286.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game1/precache-manifest.20a2ce7db4773b070d8cfd49fdff60e1.js"
+  "/game1/precache-manifest.23199c0fa5fbbde1c85fe3298f741773.js"
 );
 
 self.addEventListener('message', (event) => {
