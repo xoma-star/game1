08c5d7d4f4942bc8546f455c33463869260eb272
xoma_star <40947642+xoma-star@users.noreply.github.com>
2020-07-12 20:27:48 +0500

    Updates

---
+++ precache-manifest.2971fd6286af15614dc78da49a2e7a0f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec691e7cc068fd19297f895171d74e46",
+    "url": "/game1/index.html"
+  },
+  {
+    "revision": "4ad105d4b4b087b5415f",
+    "url": "/game1/static/css/2.a140de60.chunk.css"
+  },
+  {
+    "revision": "9e4957d1f4b4cb458960",
+    "url": "/game1/static/css/main.70c4fd54.chunk.css"
+  },
+  {
+    "revision": "4ad105d4b4b087b5415f",
+    "url": "/game1/static/js/2.5a399a81.chunk.js"
+  },
+  {
+    "revision": "fcf5ed6a91207ed3cdc9cbbcc9f389d9",
+    "url": "/game1/static/js/2.5a399a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4957d1f4b4cb458960",
+    "url": "/game1/static/js/main.7cb31c50.chunk.js"
+  },
+  {
+    "revision": "de184dbccaf894dd332d",
+    "url": "/game1/static/js/runtime-main.0f06a286.js"
+  },
+  {
+    "revision": "53770ec27038ba27fe2c6122c577a0c6",
+    "url": "/game1/static/media/UMPO.53770ec2.png"
+  },
+  {
+    "revision": "910d5316ecec7c417ff8f4812d1e53ff",
+    "url": "/game1/static/media/pv.910d5316.png"
+  },
+  {
+    "revision": "59a4c6da1253d4500f9456a184d51d3e",
+    "url": "/game1/static/media/wh.59a4c6da.png"
+  }
+]);
