7229bfa1e3eec606e0665894d15019876dc1e6b3
xoma_star <40947642+xoma-star@users.noreply.github.com>
2020-07-12 19:03:38 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game1/precache-manifest.24a62605dbe5d1fc9a3cfb32a83348fc.js"
+  "/game1/precache-manifest.f7559f5b1024d836b46938f624c08672.js"
 );
 
 self.addEventListener('message', (event) => {
