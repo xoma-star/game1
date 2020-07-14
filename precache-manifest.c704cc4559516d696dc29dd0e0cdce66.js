07df0b08108d19bf18d61d7eeb9678c51b9b5c6a
xoma_star <40947642+xoma-star@users.noreply.github.com>
2020-07-14 09:22:33 +0500

    Updates

---
+++ precache-manifest.c704cc4559516d696dc29dd0e0cdce66.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2440a5ac69053dcf0ae93f83301273",
+    "url": "/game1/index.html"
+  },
+  {
+    "revision": "a9ac9a4e9998d765560a",
+    "url": "/game1/static/css/2.a140de60.chunk.css"
+  },
+  {
+    "revision": "18eccc1e90e3012d8c1a",
+    "url": "/game1/static/css/main.70c4fd54.chunk.css"
+  },
+  {
+    "revision": "a9ac9a4e9998d765560a",
+    "url": "/game1/static/js/2.f6542e1b.chunk.js"
+  },
+  {
+    "revision": "fcf5ed6a91207ed3cdc9cbbcc9f389d9",
+    "url": "/game1/static/js/2.f6542e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18eccc1e90e3012d8c1a",
+    "url": "/game1/static/js/main.baf877d1.chunk.js"
+  },
+  {
+    "revision": "de184dbccaf894dd332d",
+    "url": "/game1/static/js/runtime-main.0f06a286.js"
+  },
+  {
+    "revision": "53770ec27038ba27fe2c6122c577a0c6",
+    "url": "/game1/static/media/UMPO.53770ec2.png"
+  },
+  {
+    "revision": "910d5316ecec7c417ff8f4812d1e53ff",
+    "url": "/game1/static/media/pv.910d5316.png"
+  },
+  {
+    "revision": "59a4c6da1253d4500f9456a184d51d3e",
+    "url": "/game1/static/media/wh.59a4c6da.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/game1/precache-manifest.8aaac142a22e7ddda3d2c3715ef098b4.js"
+  "/game1/precache-manifest.c704cc4559516d696dc29dd0e0cdce66.js"
 );
 
 self.addEventListener('message', (event) => {
